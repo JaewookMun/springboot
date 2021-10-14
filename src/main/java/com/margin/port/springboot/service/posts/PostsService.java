34e4d00b14a9587cf468675614d223e59d4480d9
JaewookMun <mun1103.dev@gmail.com>
2021-10-15 00:22:08 +0900

    jpa를 활용한 게시글 저장 기능 구현

---
+++ src/main/java/com/margin/port/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.margin.port.springboot.service.posts;
+
+import com.margin.port.springboot.domain.posts.PostsRepository;
+import com.margin.port.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
