7b25261d1c52dae058c575138d3c802132140e81
JaewookMun <mun1103.dev@gmail.com>
2021-10-15 23:48:58 +0900

    jpa update/read function

--- src/main/java/com/margin/port/springboot/service/posts/PostsService.java
+++ src/main/java/com/margin/port/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.margin.port.springboot.service.posts;
 
+import com.margin.port.springboot.domain.posts.Posts;
 import com.margin.port.springboot.domain.posts.PostsRepository;
+import com.margin.port.springboot.web.dto.PostsResponseDto;
 import com.margin.port.springboot.web.dto.PostsSaveRequestDto;
+import com.margin.port.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,24 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
+
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts=postsRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()
+                ->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
